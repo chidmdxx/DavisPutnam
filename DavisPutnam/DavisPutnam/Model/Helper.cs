087a4aea225715b24beb268f4f86115d2966da4b
santorab <rodrigo.santoyorn@udlap.mx>
2014-05-07 13:29:38 -0500

    Update Helper.cs

--- DavisPutnam/DavisPutnam/Model/Helper.cs
+++ DavisPutnam/DavisPutnam/Model/Helper.cs
@@ -74,7 +74,7 @@ namespace DavisPutnam.Model
                         var gamaPrima = g1.Join(g2);
                         if (!gamaPrima.Tautologia())
                         {
-                            deltaPrima = Clause.Concat(deltaPrima, gamaPrima);
+                            deltaPrima = deltaPrima.Join(gamaPrima);
                         }
                     }
                 }
