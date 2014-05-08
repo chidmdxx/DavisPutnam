f7bd2397158b500e84e390f7dc26c3370d59c5c5
chidmdxx <rachid@outlook.com>
2014-05-08 01:07:21 -0500

    distribution y operators out

--- DavisPutnam/DavisPutnam/Model/Transformar.cs
+++ DavisPutnam/DavisPutnam/Model/Transformar.cs
@@ -22,7 +22,7 @@ namespace DavisPutnam.Model
             OperatorsOut();
         }
 
-        public void ImplicationsOut()
+        private void ImplicationsOut()
         {
             int index = 0;
             int indexAuxiliar = 0;
@@ -66,7 +66,7 @@ namespace DavisPutnam.Model
                 }
             }
         }
-        public void ReductionsOut()
+        private void ReductionsOut()
         {
             int index = 0;
             while (true)
@@ -82,7 +82,7 @@ namespace DavisPutnam.Model
                 Enunciado = Enunciado.Insert(index + 1, "!");
             }
         }
-        public void NegationsIn()
+        private void NegationsIn()
         {
             int index = -1;
             int indexAuxiliar = 0;
@@ -140,14 +140,139 @@ namespace DavisPutnam.Model
             Enunciado = Enunciado.Replace("!!", "");
         }
 
-        public void Distribution()
+        private void Distribution()
         {
-
+            while (true)
+            {
+                var inicioParentesis = -1;
+                var finParentesis = -1;
+                var siguienteParentesis = -1;
+                var indiceElementoAnterior = -1;
+                var indiceElementoSiguiente = -1;
+                while (true)
+                {
+                    inicioParentesis = Enunciado.IndexOf('(', inicioParentesis + 1);
+                    siguienteParentesis = Enunciado.IndexOf('(', inicioParentesis + 1);
+                    finParentesis = Enunciado.IndexOf(')', finParentesis + 1);
+                    if (siguienteParentesis == -1 || siguienteParentesis > finParentesis)
+                    {
+                        break;
+                    }
+                }
+                if (inicioParentesis == -1)
+                {
+                    break;
+                }
+                indiceElementoAnterior = inicioParentesis - 1;
+                indiceElementoSiguiente = finParentesis + 1;
+                do
+                {
+                    indiceElementoAnterior--;
+                } while (Enunciado[indiceElementoAnterior] != '&' && Enunciado[indiceElementoAnterior] != '|');
+                do
+                {
+                    indiceElementoSiguiente++;
+                } while (Enunciado[indiceElementoAnterior] != '&' && Enunciado[indiceElementoAnterior] != '|');
+                var subEnunciado = Enunciado.Substring(indiceElementoAnterior, indiceElementoSiguiente - indiceElementoAnterior);
+                Enunciado = Enunciado.Replace(subEnunciado, HacerDistribucion(subEnunciado));
+            }
         }
 
-        public void OperatorsOut()
+        private string HacerDistribucion(string enunciado)
         {
+            string toReturn = enunciado.Replace('(', '[').Replace(')', ']');
+            var inicioParentesis = enunciado.IndexOf('(');
+            var finParentesis = enunciado.IndexOf(')');
+            var dentroParentesis = enunciado.Substring(inicioParentesis, finParentesis - inicioParentesis).Trim('(', ')');
+            var operadorAntes = enunciado[inicioParentesis - 1];
+            var operadorDespues = enunciado[finParentesis + 1];
+            char operadorDentro;
+            string elementoAntes = enunciado.Substring(0, inicioParentesis - 1).Trim(operadorAntes);
+            string elementoDespues = enunciado.Substring(finParentesis + 1).Trim(operadorDespues);
+            bool mismoAdentro = (dentroParentesis.Contains('&') && !dentroParentesis.Contains('|')) ||
+                (dentroParentesis.Contains('|') && !dentroParentesis.Contains('&'));
+            if (mismoAdentro)
+            {
+                var builder = new StringBuilder();
+                operadorDentro = dentroParentesis.Contains('&') ? '&' : '|';
+                if (operadorDentro == '&' && operadorAntes == '|')
+                {
+                    builder.Append('(');
+                    var allElementos = dentroParentesis.Split(operadorDentro);
+                    foreach (var s in allElementos)
+                    {
+                        builder.AppendFormat("{0}|{1}&", elementoAntes, s);
+                    }
+                    builder.Append(')');
+                    builder.Replace("&)", ")");
+                    builder.AppendFormat("{0}{1}", operadorDespues, elementoDespues);
+                }
+                else if (operadorDentro == '&' && operadorDespues == '|')
+                {
+                    builder.AppendFormat("{0}{1}", elementoAntes, operadorAntes);
+                    builder.Append('(');
+                    var allElementos = dentroParentesis.Split(operadorDentro);
+                    foreach (var s in allElementos)
+                    {
+                        builder.AppendFormat("{0}|{1}&", elementoDespues, s);
+                    }
+                    builder.Append(')');
+                    builder.Replace("&)", ")");
+                }
+                else if (operadorAntes == operadorDentro && operadorDentro == operadorDespues)
+                {
+                    toReturn.Replace("[", "").Replace("]", "");
+                    builder.Append(toReturn);
+                }
+                else if (operadorAntes == operadorDentro)
+                {
+                    builder.Append('(');
+                    builder.AppendFormat("{0}{1}", elementoAntes, operadorAntes);
+                    var allElementos = dentroParentesis.Split(operadorDentro);
+                    foreach (var s in allElementos)
+                    {
+                        builder.AppendFormat("{0}{1}", s, operadorDentro);
+                    }
+                    builder.Append(')');
+                    builder.Replace(operadorDentro + ")", ")");
+                    builder.AppendFormat("{0}{1}", operadorDespues, elementoDespues);
+                }
+                else if(operadorDespues==operadorDentro)
+                {
+                    builder.AppendFormat("{0}{1}", elementoAntes, operadorAntes);
+                    builder.Append('(');
+                    var allElementos = dentroParentesis.Split(operadorDentro);
+                    foreach (var s in allElementos)
+                    {
+                        builder.AppendFormat("{0}{1}", s, operadorDentro);
+                    }
+                    builder.Append(elementoDespues);
+                    builder.Append(')');
+                }
+                else
+                {
+                    builder.Append(toReturn);
+                }
+                toReturn = builder.ToString();
+            }
 
+            return toReturn;
+        }
+
+        private void OperatorsOut()
+        {
+            Resultado = new List<Clause>();
+            var enunciadoSplit = Enunciado.Split('&', '[', ']');
+            foreach (var clause in enunciadoSplit)
+            {
+                var elementos = clause.Split('|');
+                Clause temp=new Clause();
+                foreach(var el in elementos)
+                {
+                    temp.AddElement(el);
+                }
+                Resultado.Add(temp);
+            }
         }
     }
 }
